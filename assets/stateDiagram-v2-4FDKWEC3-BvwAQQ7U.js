76c8d43e7f8dc33204ff515833c2ac666077e757
DaviMarino <davi.marino@outlook.com>
2026-01-20 20:20:23 -0300

    Updates

---
+++ assets/stateDiagram-v2-4FDKWEC3-BvwAQQ7U.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-DI55MBZ5-B-QS0A5Q.js";import{_ as i}from"./index-Zi4XHhUO.js";import"./chunk-55IACEB6-C5WBHS1a.js";import"./chunk-QN33PNHL-B8gITlMe.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
