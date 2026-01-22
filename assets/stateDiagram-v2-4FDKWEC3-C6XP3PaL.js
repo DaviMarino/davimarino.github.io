1e4ffc8f0a02e557e91d7c57eaeaf780885bb9b4
DaviMarino <davi.marino@outlook.com>
2026-01-22 19:46:25 -0300

    Updates

--- assets/stateDiagram-v2-4FDKWEC3-C6XP3PaL.js
+++
@@ -1,1 +0,0 @@
-import{s as t,b as r,a,S as s}from"./chunk-DI55MBZ5-DqRuVaG4.js";import{_ as i}from"./index-CBqhf8Dk.js";import"./chunk-55IACEB6-Cvu3-f3Q.js";import"./chunk-QN33PNHL-Bir3oEtl.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
