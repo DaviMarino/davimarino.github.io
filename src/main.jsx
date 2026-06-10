cfc5a2f97008fcacff784984f369cf5d77712996
Davi Marino <davi.marino@outlook.com>
2026-06-10 20:20:55 -0300

    fix: unregister old service workers on load

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// Limpa todo cache antigo de service workers
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    regs.forEach(reg => reg.unregister())
+  })
+}
+
 import { registerSW } from 'virtual:pwa-register'
 
 registerSW({ immediate: true })
